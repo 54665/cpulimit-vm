186d3cf199a4fd4c7cde21149fba51a2b7a2b1a5
54665 <45261464+54665@users.noreply.github.com>
2020-11-25 11:16:22 +0800

    vm cpu限制初代脚本
    vm cpu限制初代脚本

---
+++ kvm.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#
+# AWS流量阈值脚本副脚本
+#
+# Copyright (c) 2020.
+
+echo "开始安装CPU限制程序"
+yum install epel-release cpulimit
+echo "开始下载CPU限制脚本"
+wget https://raw.githubusercontent.com/54665/cpulimit-vm/master/cpulimit.sh
+chmod +x /root/cpulimit.sh
+echo "写入自启"
+chmod +x /etc/rc.d/rc.local
+echo "/root/cpulimit-vm.sh >/root/cpulimit-vm.sh/cpulimit.sh.log 2>&1" >> /etc/rc.d/rc.local
+echo "后台运行中"
+nohup /root/cpulimit-vm.sh >> /dev/null 2>&1 &
+echo "脚本执行完毕，默认基准20%，突发值40%后开始限制"
